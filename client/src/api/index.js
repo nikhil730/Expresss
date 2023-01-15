3cfd64f20b372abe5e7f0e8cdb7464bcafef2f7d
nikhil730 <nikhilbhalla196@gmail.com>
2023-01-15 17:05:40 +0530

    .

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://expresss-x6my.onrender.com/" });
+const API = axios.create({ baseURL: "http://localhost:5000" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
