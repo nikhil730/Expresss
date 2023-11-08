7447dd5e562d88bcf1f683979dea32d2493a7444
nikhil730 <nikhilbhalla196@gmail.com>
2023-11-09 01:18:43 +0530

    changes made according to dev branch

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://express-backend-cs79.onrender.com" });
+const API = axios.create({ baseURL: "http://localhost:5000" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
